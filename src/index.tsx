bee7374e0415ff28a00f591d4a70a4c9d3b3a0a3
asfandyarjalil1 <asfandyarjalil@vizzwebsolutions.com>
2024-01-12 16:02:06 +0500

    integrate all chains  that are require by client

--- src/index.tsx
+++ src/index.tsx
@@ -5,24 +5,61 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@rainbow-me/rainbowkit/styles.css';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
-import {Chain,ConnectButton,getDefaultWallets,lightTheme,midnightTheme,RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
+import {Chain,ConnectButton,getDefaultWallets,lightTheme,midnightTheme,RainbowKitProvider,} from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig ,useConfig} from 'wagmi';
-import {mainnet,polygon,optimism,arbitrum,base,} from 'wagmi/chains';
+import {mainnet,polygon,optimism,arbitrum,base,sepolia,avalanche,bsc,zkSync,polygonZkEvm,linea,gnosis,fantom,moonriver,moonbeam,boba,aurora,fuse} from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
-
+import { publicProvider } from 'wagmi/providers/public'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+
+// Congigure all chains that are require also set manually icon of some chains
+
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base,],
-  [
-    alchemyProvider({ apiKey: String(process.env.REACT_APP_ALCHEMY_ID) }),  
-  ]
+  [mainnet, polygon, optimism, arbitrum, base, sepolia, avalanche, bsc, zkSync,
+    {
+      ...aurora,
+      iconUrl: 'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/chains/aurora.png',
+    },
+    {
+      ...boba,
+      iconUrl:'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/chains/boba.png'
+    },
+    {
+      ...fuse,
+      iconUrl:'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/chains/fuse.svg'
+    },
+    {
+      ...moonbeam,
+      iconUrl:'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/chains/moonbeam.svg'
+    },
+    {
+      ...moonriver,
+      iconUrl:'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/chains/moonriver.svg',
+    },
+    {
+      ...fantom,
+      iconUrl:'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/chains/fantom.svg'
+    }, {
+      ...gnosis,
+      iconUrl:'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/chains/gnosis.svg',
+    }, {
+      ...linea,
+      iconUrl:'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/chains/linea.svg',
+    }
+  ],
+  // [
+  //   alchemyProvider({ apiKey: String(process.env.REACT_APP_ALCHEMY_ID) }),
+  // ]
+  [publicProvider()]
 );
 
+// END
+
+// calling getdedault function to get chain from wagmi/chains and passing it into the configuration
 
 const { connectors } = getDefaultWallets({
   appName: 'My RainbowKit App',
@@ -30,11 +67,18 @@ const { connectors } = getDefaultWallets({
   chains
 });
 
+// END
+
+// calling createConfig function that pass connectors and publicclient  to make connect with blockchain
+
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
   publicClient
 })
+
+// END
+
 
 root.render(
   <React.StrictMode>
@@ -47,3 +91,4 @@ root.render(
 );
 
 reportWebVitals();
+
